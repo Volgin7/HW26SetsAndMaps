33d3af3d60a1c95b8a2cdb490b36224a6053afd2
Volgin7 <8612020@gmail.com>
2023-05-17 22:14:47 +0300

    +hw final

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
         task1();
         task2();
         task3();
         task4();
-
     }
 public static void task1() {
     List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
