319defc02ce3058566f6a04079747a0800321551
Volgin7 <8612020@gmail.com>
2023-05-17 20:56:15 +0300

    +task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        task1();
     }
+public static void task1() {
+    List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+
+    for (Integer num : nums) {
+        if(num % 2 ==0) {
+            System.out.println(num);
+        }
+    }
+
+} // end of task1
+
+
+
 }
